445b313d4b2d14da25a52a3ba8faecc37cd33374
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-30 16:48:32 -0700

    Created a new instance and able to render using underscore template

--- src/app.js
+++ src/app.js
@@ -9,4 +9,37 @@
 
 import $ from 'jquery';
 import _ from 'underscore';
+// Link to contact model
 import Contact from './app/models/contact.js';
+
+// Creating instance of Contact model with static data
+var myContact = new Contact({
+  name: "Rana Sulaiman",
+  Email: "rs_sulaiman@yahoo",
+  Phone: '435-333-777'
+});
+
+var render = function(contact){
+  // selecting everything in script tag and assigning it to var and convert it to jq so we can use html function
+  var templateText = $('#tmpl-contact-card').html();
+
+  // Create a template object
+  var templateObject = _.template(templateText);
+
+  // Complile a template object/in trek we serialize the data because we were sending it to API
+  // This is closure, we are passing the function as an argument
+  var compiledHTML = templateObject(contact.toJSON());
+
+  // Append result to the DOM
+$('#contact-cards').append(compiledHTML);
+
+
+};
+
+
+
+$(document).ready(function() {
+  console.log(myContact);
+  render(myContact);
+
+});
