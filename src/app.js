6892b5f7846165292ae07d542646d7372402a13c
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-30 15:45:30 -0700

    Initaila setup for app.js. imported jQuery, underscore and contact model

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-import Application from 'app/models/application';
-import ApplicationView from 'app/views/application_view';
+// import Application from 'app/models/application';
+// import ApplicationView from 'app/views/application_view';
+// var application = new Application();
+//
+// var appView = new ApplicationView({
+//   el: '#application',
+//   model: application
+// });
 
-var application = new Application();
-
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
-});
+import $ from 'jquery';
+import _ from 'underscore';
+import Contact from './app/models/contact.js';
