908b419e00c5065cb42298885422e3cbfa9551e9
Anacelia Gillette <acgillette426@gmail.com>
2017-05-30 15:50:46 -0700

    Added a render function and now a single contact shows

--- src/app.js
+++ src/app.js
@@ -6,7 +6,28 @@ import Contact from './app/models/contact.js';
 
 var application = new Application();
 
+var myContact = new Contact({
+  name: "A name",
+  phone_num: "3057778888",
+  email: "email@email.com"
+});
+
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+var render = function(contact) {
+  var templateText = $('#tmpl-contact-card').html();
+
+  var templateObject = _.template(templateText);
+
+  var compiledHTML = $(templateObject(contact.toJSON()));
+
+  $('#contact-cards').append(compiledHTML);
+
+};
+
+$(document).ready(function() {
+  render(myContact);
 });
