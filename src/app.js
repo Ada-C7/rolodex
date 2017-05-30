7d3fd458b32544213897ca21f587108155e1a448
Spatterjaaay <aja.pursova@gmail.com>
2017-05-30 15:44:32 -0700

    added imports to app.js and hardcoded some initial contacts

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import $ from 'jquery';
+import _ from 'underscore';
+import Contact from './models/contact.js';
+import Rolodex from './collections/rolodex.js';
 
 var application = new Application();
 
@@ -7,3 +11,19 @@ var appView = new ApplicationView({
   el: '#application',
   model: application
 });
+
+var contactData = [
+  {
+    name: "Bob Belcher",
+    phone: "12x3456",
+    email: "bob@burgers.com"
+  }, {
+    name: "Louise Belcher",
+    phone: "345x6789",
+    email: "louise@burgers.com"
+  }, {
+    name: "Tina Belcher",
+    phone: "0987654x",
+    email: "tina@burgers.com"
+  }
+];
