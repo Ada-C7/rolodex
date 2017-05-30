3220a16b1604e8e545ae599e5a7d7a7dcf72eda1
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-30 16:53:38 -0700

    added form code, need to make buttons functional

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
-import Contact from 'app/models/contact.js';
 import $ from 'jquery';
 import _ from 'underscore';
+import Contact from 'app/models/contact.js';
+import Rolodex from 'app/collections/rolodex.js';
+
 
 var contactData = [
   {
-    name: "Joe",
-    email: "joe_blow86@example.com",
-    phone: "444-555-6666"
+    name: "John",
+    email: "john@example.com",
+    phone: "123-456-7890"
   }
 ];
+
+var myRolodex = new Rolodex(contactData);
 
 
 var myContact = new Contact({
@@ -18,6 +22,30 @@ var myContact = new Contact({
   email: "sue_shmo86@example.com",
   phone: "444-555-7777"
 });
+
+// var myOtherContact = new Contact({
+//   name: "sadlkf",
+//   email: "asdfdsafdsasdafasdfsad@example.com",
+//   phone: "444-555-7077"
+// });
+
+
+var getFormData = function() {
+  var formName = $("name").val();
+  $("#name").val('');
+
+  var formEmail = $("#email").val();
+  $("#email").val('');
+
+  var formPhone = $("#phone").val();
+  $("#phone").val('');
+
+  return {
+    name: formName,
+    email: formEmail,
+    phone: formPhone
+  };
+};
 
 var render = function(contact) {
   var templateName = $('#tmpl-contact-card').html();
@@ -28,8 +56,11 @@ var render = function(contact) {
 
   $('#contact-cards').append(compiledHTML);
 
-};
+  compiledHTML.find('button.success').click({contactToAdd: contact}, function(params) {
+    myRolodex.remove(params.data.contactToAdd);
+  });
 
+};
 
 var application = new Application();
 
@@ -39,6 +70,20 @@ var appView = new ApplicationView({
 });
 
 
+var renderList = function(rolodex) {
+  $("#contact-cards").empty();
+
+  rolodex.each(function(contact) {
+    render(contact);
+  });
+};
+
 $(document).ready(function() {
   render(myContact);
+  // render(myOtherContact);
+
+  $("#add-contact").click(function() {
+    var contact = new Contact(getFormData());
+
+  });
 });
