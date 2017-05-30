d904301de4bcc398dc48e3938d15260dab4c297d
Anacelia Gillette <acgillette426@gmail.com>
2017-05-30 15:22:39 -0700

    Importing initial files for app.js + made contact model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import $ from 'jquery';
+import _ from 'underscore';
+import Contact from './app/models/contact.js';
 
 var application = new Application();
 
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,17 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+  defaults: {
+    'name': '',
+    'phone_num': '',
+    'email': ''
+  },
+
+  validate: function(attrs) {
+    if(!attrs.name) {
+      return 'Please enter a name';
+    }
+  }
 });
 
 export default Contact;
