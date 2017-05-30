864127b1eab16aca8aa0a6dc73729a71ad6899e5
Ann Dai <gofarann@gmail.com>
2017-05-30 15:16:51 -0700

    created Contact model and imported to app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import $ from 'jquery';
+import _ from 'underscore';
+import Contact from './app/models/contact.js';
 
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+
+var newContact = new Contact({
+  name: "Ann Dai",
+  email: "gofarann@gmail.com",
+  phone: "701-306-7504"
+});
+
+var renderCard = function(contact){
+  console.log(contact);
+};
+
+$(document).ready(function() {
+  renderCard("new contact");
+
 });
