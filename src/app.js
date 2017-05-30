78a5ac91677fd3404c7138b522f782bd3b7e90d1
natalia-ku <bilberryfm@mail.ru>
2017-05-30 16:58:02 -0700

    new form is working

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
-
+//===================================
 import _ from 'underscore';
 import $ from 'jquery';
 // import 'jquery-colpick';
 import Contact from 'app/models/contact';
+import Rolodex from 'app/collections/rolodex';
 
-
+//===================================
 var application = new Application();
 
 var appView = new ApplicationView({
@@ -30,24 +31,63 @@ var contactData = [{
   name: "David",
   phone: "322323323",
   email: "dav@com"
+},
+{
+  name: "SomeName",
+  phone: "12345766",
+  email: "a123@com"
 }];
 
 
+var  contactList = new Rolodex(contactData);
 
-var render = function(contact, template){
-  var generatedHTML = template(contact.toJSON()); // give template data to generate html
-  $('#contact-cards').append(generatedHTML);
+var readNewContactForm = function() {
+  var formName= $('#name').val();
+  $('#name').val(''); //reset a form, set all textboxes to empty
+  var formEmail= $('#email').val();
+  $('#email').val('');
+  var formPhone= $('#phone').val();
+  $('#phone').val('');
+
+  return {
+    name: formName,
+    email: formEmail,
+    phone: formPhone
+  };
 };
+
+
+
+var render = function(contact){
+  var generatedHTML = contactDetailsTemplate(contact.toJSON()); // give template data to generate html
+    $('#contact-cards').append(generatedHTML);
+
+};
+
+
+var renderList = function(contactList) {
+  $('#contact-cards').empty();
+  contactList.each(function(contact) {
+    render(contact);
+  });
+};
+
 
 
 $(document).ready(function() {
   contactDetailsTemplate = _.template($('#tmpl-contact-details').html());
-  contactTemplate = _.template($('#tmpl-contact-card').html());
-  var contact = new Contact(contactData[0])
-  render(contact, contactTemplate);
-  render(contact, contactDetailsTemplate);
+  // contactTemplate = _.template($('#tmpl-contact-card').html());
 
 
+  $('.add-contact').click( function(){
+    var contact = new Contact(readNewContactForm() );
+    contactList.add(contact);
+  });
+  renderList(contactList);
+
+  contactList.on("update", function() {
+    renderList(contactList);
+  });
 
 
 
