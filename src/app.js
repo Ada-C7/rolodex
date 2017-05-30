a14c3741c11319ec557c3e8247e87fce862005e3
Lynn Trickey <lynn.trickey@gmail.com>
2017-05-30 15:59:45 -0700

    Created new collection, able to console.log

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ApplicationView from 'app/views/application_view';
 import $ from 'jquery';
 import _ from 'underscore';
 import Contact from './app/models/contact.js';
+import Rolodex from './app/collections/rolodex.js';
 
 var application = new Application();
 
@@ -17,6 +18,26 @@ var myContact = new Contact({
   email: "lynn@test.com"
 });
 
+var contactData = [
+  {
+    name: "Mom",
+    phone: 2063658186,
+    email: "mom@test.com"
+  },
+  {
+    name: "Lynn",
+    phone: 2062401029,
+    email: "lynn@test.com"
+  },
+  {
+    name: "Annie",
+    phone: 2061234589,
+    email: "Annie@test.com"
+  }
+];
+
+var myRolodex = new Rolodex(contactData);
+
 var render = function(contact) {
   var templateText = $("#tmpl-contact-card").html();
 
@@ -27,8 +48,15 @@ var render = function(contact) {
   $("#contact-cards").append(compiledHTML);
 };
 
+var renderCollection = function(collection) {
+  $("#contact-cards").empty();
+  collection.each(function(contact) {
+    render(contact);
+  });
+};
+
 $(document).ready(function() {
   $("#contact-details").hide();
-  console.log(myContact);
-  render(myContact);
+  console.log(myRolodex);
+  // render(myContact);
 });
