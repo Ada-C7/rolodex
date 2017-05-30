b29904ce1c7a52fd82297a4a8381ad8e06f24dbf
AlisonZ <alisonkzerbe@gmail.com>
2017-05-30 16:52:33 -0700

    added single contanct card

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import $ from 'jQuery';
+import _ from 'underscore';
+import Contact from './app/models/contact.js';
 
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+var myContact = new Contact({
+    name: 'Mom',
+    phoneNumber: '123',
+    email: 'mom@gmail.com'
+})
+
+var render = function(contact){
+    var templateText = $('#tmpl-contact-card').html();
+    var templateObject = _.template(templateText);
+
+    var compiledHTML = $(templateObject(contact.toJSON()));
+
+    $('#contact-cards').append(compiledHTML);
+}
+
+
+$(document).ready(function(){
+    console.log('hi there');
+    console.log(myContact);
+    render(myContact);
 });
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -1,8 +1,14 @@
 import Backbone from 'backbone';
 
 const Contact = Backbone.Model.extend({
-  // This model should have the attributes for
-  // a single contact: name, phone number, and email.
+    // This model should have the attributes for
+    // a single contact: name, phone number, and email.
+
+    defaults: {
+        name: '',
+        phoneNumber: 0,
+        email: ''
+    }
 });
 
 export default Contact;
