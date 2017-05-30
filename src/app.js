fbe0ae421eda427b8fdff28f760367e6e72419e7
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-30 16:15:01 -0700

    Wave 1 complete. Displays manually created contact

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
-import Application from 'app/models/application';
-import ApplicationView from 'app/views/application_view';
+import $ from 'jquery';
+import _ from 'underscore';
+import Contact from '/app/models/contact';
+// import Application from './src/app/models/application';
+// import ApplicationView from './src/app/views/application_view';
 
-var application = new Application();
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
+// WAVE 1
+var myContact = new Contact({
+  name: "chris",
+  phone: "8005553333",
+  email: "notreal@gmail.com"
+});
+
+var renderContact = function(contact){
+
+  var contactTemplate = _.template($('#tmpl-contact-card').html());
+  var contactDetails = contactTemplate({
+    name: contact.get("name")
+  });
+  $('#contact-cards').append($(contactDetails));
+};
+
+$(document).ready(function() {
+  renderContact(myContact);
 });
