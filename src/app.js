e01953778bd14f432954bc9c1965f56178545d37
add2point71dots <alcland@gmail.com>
2017-05-30 16:00:21 -0700

    renders collection of contacts

--- src/app.js
+++ src/app.js
@@ -1,9 +1,51 @@
+import $ from 'jquery';
+import _ from 'underscore';
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import Contact from './app/models/contact.js';
+import Rolodex from './app/collections/rolodex.js';
 
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+var contactData = [
+  {
+    name: 'Ada',
+    email: 'ada@example.com',
+    phone: '555-555-5555'
+  },
+  {
+    name: 'Galois',
+    email: 'galois@example.com',
+    phone: '444-555-5555'
+  },
+  {
+    name: 'Turing',
+    email: 'turing@example.com',
+    phone: '333-555-5555'
+  }
+];
+
+var myContactList = new Rolodex(contactData);
+
+var render = function(contact) {
+  var templateText = $('#tmpl-contact-card').html();
+  var templateObject = _.template(templateText);
+  var compiledHTML = $(templateObject(contact.toJSON()));
+  $('#contact-cards').append(compiledHTML);
+};
+
+var renderList = function(contactList) {
+  $('#contact-cards').empty();
+  contactList.each(function(contact) {
+    render(contact);
+  });
+};
+
+$(document).ready(function() {
+  renderList(myContactList);
 });
