0d68494c0bda8f3ba1d14181dbe315261f11b6c6
jjousun <jjousun@gmail.com>
2017-05-30 16:30:28 -0700

    Rendered one Contact instance

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import $ from 'jquery';
 import _ from 'underscore';
-import Contact from './models/contact.js';
+import Contact from './app/models/contact.js';
 
+var myContact = new Contact({
+  name: "Ada",
+  email: "ada@ada.org",
+  phone: 6172779728
+});
+
+var render = function(Contact) {
+  var templateText = $('#tmpl-contact-card').html();
+
+  var templateObject = _.template(templateText);
+
+  var compiledHTML = templateObject(Contact.toJSON());
+
+  $("#contact-cards").append(compiledHTML);
+};
 
 $(document).ready(function() {
+  // console.log(myContact.get("name"));
+  render(myContact);
 
 });
