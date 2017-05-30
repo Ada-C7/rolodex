b601c905f36623de805b0b7b6c15ec7a0e73864e
sai samant <saisamant@gmail.com>
2017-05-30 16:05:07 -0700

    adding imports to app and document.ready

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
+import _ from 'underscore';
+import $ from 'jquery';
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import Contact from 'app/models/contact.js';
+import Rolodex from 'app/collections/rolodex.js';
 
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+
+
+
+$(document).ready(function(){
+
 });
--- src/app/collections/rolodex.js
+++ src/app/collections/rolodex.js
@@ -1,6 +1,7 @@
 import Backbone from 'backbone';
 
 const Rolodex = Backbone.Collection.extend({
+  model: Contact
   // This Rolodex represents a collection of Contacts
   // and should include any methods or attributes
   // that are involved in working with more than one
