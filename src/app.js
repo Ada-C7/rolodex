0acab794a0f4357790591cec77bd5ad5ade8ca6f
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-30 15:49:38 -0700

    added functions and Contact model, need to display contact from static info

--- src/app.js
+++ src/app.js
@@ -1,9 +1,42 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import Contact from 'app/models/contact.js';
+import $ from 'jquery';
+import _ from 'underscore';
+
+var contactData = [
+  {
+    name: "Joe",
+    email: "joe_blow86@example.com",
+    phone: "444-555-6666"
+  }
+];
+
+
+var myContact = new Contact({
+  name: "Sue",
+  email: "sue_shmo86@example.com",
+  phone: "444-555-7777"
+});
+
+var render = function(contact) {
+  var templateName = $('#tmpl-contact-card').html();
+
+  var templateObject = _.template(templateName);
+
+  var compiledHTML = (templateObject(contact.toJSON()));
+
+};
+
 
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+
+$(document).ready(function() {
+  render(myContact);
 });
