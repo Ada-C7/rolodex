af9662a98e5665cd232b2170587965ba612db9ab
Brenna <bcmdarroch@gmail.com>
2017-05-30 15:46:25 -0700

    set up defaults for contact

--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,11 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+  defaults: {
+    name: "It's a me",
+    phone: "333-777-8888",
+    email: "me@me.com"
+  }
 });
 
 export default Contact;
