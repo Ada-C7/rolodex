419c39f97ae2cb092388a923c6cc9df35a643fca
natalia-ku <bilberryfm@mail.ru>
2017-05-30 16:18:34 -0700

    wave 1 is completed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,55 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
+import _ from 'underscore';
+import $ from 'jquery';
+// import 'jquery-colpick';
+import Contact from 'app/models/contact';
+
+
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+
+//===================================
+
+var contactDetailsTemplate;
+var contactTemplate;
+// var contactTemplate;
+
+var contactData = [{
+  name: "Natalia",
+  phone: "232332332",
+  email: "nat@com"
+},
+{
+  name: "David",
+  phone: "322323323",
+  email: "dav@com"
+}];
+
+
+
+var render = function(contact, template){
+  var generatedHTML = template(contact.toJSON()); // give template data to generate html
+  $('#contact-cards').append(generatedHTML);
+};
+
+
+$(document).ready(function() {
+  contactDetailsTemplate = _.template($('#tmpl-contact-details').html());
+  contactTemplate = _.template($('#tmpl-contact-card').html());
+  var contact = new Contact(contactData[0])
+  render(contact, contactTemplate);
+  render(contact, contactDetailsTemplate);
+
+
+
+
+
+
 });
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -1,6 +1,11 @@
 import Backbone from 'backbone';
 
 const Contact = Backbone.Model.extend({
+  defaults: { // THIS IS FOR DEFAULT TASK, when we creating new task witouht attributes
+    name: "Default name",
+    phone: "Default phone number",
+    email: "Default email"
+  }
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
 });
