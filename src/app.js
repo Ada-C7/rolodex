8f9198c7d9fcfc3aa4169c36008a0b4a673378ce
Ting Wong <hellotingwong@gmail.com>
2017-05-31 10:53:42 -0700

    created Contact model and static data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
+import _ from 'underscore';
 import $ from 'jquery';
+import Contact from 'app/models/contact';
+
+var myContact = new Contact({
+  name: "Ada Lovelace",
+  phone_number: "(416) 733-2221",
+  email: "Ada@adadevelopersacademy.org"
+});
+
+$(document).ready(function() {
+
+  console.log("Name: " + myContact.get("name") +  " Phone Number:" + myContact.get("phone_number") + " Email: " + myContact.get("email"));
+
+});
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,12 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+
+  defaults: {
+    "name": "Insert Name",
+    "phone number": "Insert Phone Num",
+    "email": "Insert Email"
+  }
 });
 
 export default Contact;
