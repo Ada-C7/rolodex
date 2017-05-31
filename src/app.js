f3a52ceff14bb83bdf4206f7c0006635ef789f72
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-05-30 20:52:11 -0700

    added static contact data in app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,24 @@
 import _ from 'underscore';
 import $ from 'jquery';
+
+import Contact from './models/postit';
+
+var contactData = [
+  {
+    name: 'Dara',
+    phoneNumber: '555-1212'
+    email:
+  }, {
+    name: 'Rachel',
+    phoneNumber: '207-20I-LUVU'
+    email:
+  }, {
+    name: 'Lynn',
+    phoneNumber: '555-1234'
+    email:
+  }, {
+    name: 'Jenni'
+    phoneNumber: '867-5309'
+    email:
+  }
+];
