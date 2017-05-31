4c1d5d787ec6b5bf98648764b6e3956b7bc04b9d
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-31 15:02:39 -0700

    wrote render function

--- src/app/views/rolodex.js
+++ src/app/views/rolodex.js
@@ -5,6 +5,39 @@ import ContactView from '../views/contact_view';
 import Contact from '../models/contact';
 
 var RolodexView = Backbone.View.extend({
+  initialize: function(params) {
+    this.template = params.template;
+    this.listenTo(this.model, "update", this.render);
+  },
+
+  render: function(){
+    this.$('#contact-cards').empty();
+
+    var that = this;
+    this.model.each(function(contact) {
+      var contactView = new ContactView({
+        model: contact,
+        template: that.template,
+      });
+
+      that.$('#contact-cards').append(contactView.render().$el);
+    });
+    return this;
+  },
+
+  // var renderRolodex = function(rolodex) {
+  //   // console.log("in renderRolodex");
+  //   $('#contact-cards').empty();
+  //
+  //   rolodex.each(function(contactInfo) {
+  //     var contactView = new ContactView ({
+  //       model: contactInfo,
+  //       template: templateCard
+  //     });
+  //
+  //     $('#contact-cards').append(contactView.render().$el);
+  //   });
+  // };
 
 });
 
