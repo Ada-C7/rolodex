fc52e8d863074e4a024c1b46c109e3f946a4f84a
Ting Wong <hellotingwong@gmail.com>
2017-05-31 16:53:06 -0700

    tried to create a view for contacts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import _ from 'underscore';
 import $ from 'jquery';
 import Contact from 'app/models/contact';
 import Rolodex from 'app/collections/rolodex';
+import ContactView from 'app/views/contact_view.js';
 
 var contactCardTemplate;
 var contactList;
@@ -15,6 +16,11 @@ var contactData = [{
   name: "Ting Wong",
   phone: "(416) 733-2222",
   email: "Ting@adadevelopersacademy.org"
+},
+{
+  name: "Emma",
+  phone: "(416) 733-2223",
+  email: "Emma@adadevelopersacademy.org"
 }];
 
 var render = function(contact) {
@@ -24,13 +30,41 @@ var render = function(contact) {
   $('#contact-cards').append(generatedHTML);
 };
 
+var renderList = function(contactList) {
+  // Clear the unordered list
+  $('#contact-cards').empty();
+
+  // Iterate through the list rendering each Task
+  contactList.each(function(contact) {
+    render(contact);
+
+    // Create a new TaskView with the model & template
+    var contactView = new ContactView({
+      model: Contact,
+      template: _.template($('#tmpl-contact-details').html()),
+      tagName: 'li'
+    });
+
+    // Then render the TaskView
+    // And append the resulting HTML to the DOM.
+    // $('.todo-items').append(taskView.render().$el);
+  });
+};
+
 $(document).ready(function() {
   contactCardTemplate = _.template($('#tmpl-contact-card').html());
+  contactList = new Rolodex(contactData);
 
-  contactData.forEach(function(contactData) {
-    var contact = new Contact(contactData);
-    console.log("Name: " + contact.get("name") +  " Phone Number:" + contact.get("phone") + " Email: " + contact.get("email"));
-    render(contact);
+  renderList(contactList);
+  contactList.on("update", function() {
+    renderList(contactList);
   });
+
+
+  // contactData.forEach(function(contactData) {
+  //   var contact = new Contact(contactData);
+  //   console.log("Name: " + contact.get("name") +  " Phone Number:" + contact.get("phone") + " Email: " + contact.get("email"));
+  //   render(contact);
+  // });
 
 });
