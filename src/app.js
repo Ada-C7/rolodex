bc9e23aba0739ec0b983475ac036f4522d895a10
Regan Huff <regan.huff@gmail.com>
2017-05-31 09:21:45 -0700

    Rolodex wave 1

--- src/app.js
+++ src/app.js
@@ -1,9 +1,62 @@
+import _ from 'underscore';
+import $ from 'jquery';
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
+import Contact from './app/models/contact';
+import Rolodex from './app/collections/rolodex';
+
 
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+// var newContactData = {
+//   name: 'Dr. Seuss',
+//   email: 'thing1@catinthehat.net',
+//   phone: '888-888-8888'
+// };
+//
+// var newContact = new Contact(newContactData);
+
+var newContactData = [{
+  name: 'Dr. Seuss',
+  email: 'thing1@catinthehat.net',
+  phone: '888-888-8888'
+},
+{
+  name: 'Albert Einstein',
+  email: 'smahtguy@relativity.net',
+  phone: '999-999-9999'
+},
+{
+  name: 'Amelia Earhart',
+  email: 'breezy@yahoo.com',
+  phone: '777-777-7777'
+}];
+
+var rolodex = new Rolodex(newContactData)
+
+var render = function(contact) {
+  var templateText = $('#tmpl-contact-card').html();
+  var templateObject = _.template(templateText);
+  var compiledHTML = templateObject(contact.toJSON());
+  $('#contact-cards').append(compiledHTML);
+};
+
+var renderRolodex = function(rolodex) {
+  $('#contact-cards').empty();
+  rolodex.each(function(contact) {
+    render(contact);
+  });
+};
+
+$(document).ready( function(){
+  renderRolodex(rolodex);
+
+  rolodex.on('update', function() {
+    renderRolodex(rolodex);
+  });
 });
