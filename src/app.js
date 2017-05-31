07cf5f9cb4c88e9780bd0c935369188fe35e6ef1
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-31 11:30:32 -0700

    forgot to use git. oops.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,95 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
+import $ from 'jquery';
+import _ from 'underscore';
+
+import Contact from './app/models/contact.js';
+import Rolodex from './app/collections/rolodex.js';
+import ContactView from './app/views/contact_view.js';
+
+
+
+
 var application = new Application();
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
+});
+
+var contactData = [
+  {
+    name: "Aurora",
+    phoneNumber: "555-555-555",
+    email: "fake@fakey.com"
+  },
+  {
+    name: "Felix",
+    phoneNumber: "666-666-6666",
+    email: "supafake@fakey.com"
+  }
+];
+
+var myRolodex = new Rolodex(contactData);
+
+var getFormData = function() {
+  var formName = $("#name").val();
+  $("#name").val('');
+  var formPhone = $("#phone").val();
+  $("#phone").val('');
+  var formEmail = $("#email").val();
+  $("#email").val('');
+
+  return {
+    name: formName,
+    phoneNumber: formPhone,
+    email: formEmail
+  };
+};
+
+
+
+var renderSmall = function(contact) {
+  var templateText = $('#tmpl-contact-card').html();
+  var templateObject = _.template(templateText);
+  var compiledHTML = $(templateObject(contact.toJSON()));
+
+  $('#contact-cards').append(compiledHTML);
+
+  compiledHTML.click({contact: contact}, function(params) {
+    renderSingleContact(params.data.contact);
+  });
+};
+
+var renderList = function(rolodex) {
+  $("#contact-cards").empty();
+  rolodex.each(function(contact) {
+    var contactView = new ContactView({
+      model: contact,
+      template: _.template($("#tmpl-contact-card").html())
+    });
+    $("#contact-cards").append(contactView.render().el);
+  });
+};
+
+
+$(document).ready(function() {
+  renderList(myRolodex);
+
+  myRolodex.on("update", function() {
+    renderList(myRolodex);
+  });
+
+  $("#create-contact").click(function() {
+    var newContact = new Contact(getFormData());
+    myRolodex.add(newContact);
+  });
+
+  $(".btn-cancel").click(function() {
+    $("#name").val('');
+    $("#phone").val('');
+    $("#email").val('');
+  });
+
 });
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,32 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+
+import Contact from '../models/contact.js';
+import Rolodex from '../collections/rolodex.js';
 
 const ContactView = Backbone.View.extend({
+  initialize: function(params) {
+    this.template = params.template;
+    this.listenTo(this.model, "change", this.render);
+  },
+  render: function() {
+    var compiledTemplate = this.template(this.model.toJSON());
+    this.$el.html(compiledTemplate);
+    return this;
+  },
+
+  events: {
+    "click button.btn-delete" : "removeTask",
+    // "click button.success" : "completeTask"
+  },
+
+  removeTask: function() {
+    this.model.destroy();
+  }
+
+
+
 });
 
 export default ContactView;
