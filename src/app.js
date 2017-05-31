f79d3dceb94c1d68bff4103154d8b2bc8aa2b5f9
Kayla <kubicke.kayla@gmail.com>
2017-05-31 08:59:38 -0700

    New contact can be created and added to view.
    Form can be submitted and contact will be added to collection.
    Collection of contacts displays in view and refreshes every time a new contact is added.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import Contact from '../spec/app/models/contact.js';
+import Rolodex from '../spec/app/collections/rolodex';
 
 var contactTemplate;
+var rolodex;
 
 var contactData = [
   {
-    name: "Brendon Small",
-    email:"makin_movies1996@gmail.com",
+    name: 'Brendon Small',
+    email:'makin_movies1996@gmail.com',
     phone: "222-2222"
   }
 ];
@@ -22,14 +24,52 @@ var render = function(contact) {
   $('#contact-cards').append(generatedHTML);
 };
 
+var renderRolodex = function(rolodex) {
+  $('#contact-cards').empty();
+
+  rolodex.each(function(contact) {
+    render(contact);
+  });
+};
+
+var readContactForm = function() {
+  var name = $('#name').val();
+  $('name').val('something');
+
+  var email = $('#email').val();
+  $('email').val('');
+
+  var phone = $('#phone').val();
+  $('phone').val('');
+
+  return {
+    name: name,
+    email: email,
+    phone: phone
+  };
+};
 
 $(document).ready(function() {
   contactTemplate = _.template($('#tmpl-contact-card').html());
+  rolodex = new Rolodex(contactData);
 
-  contactData.forEach(function(rawContact) {
-    var contact = new Contact(rawContact);
-    render(contact);
+  rolodex.on('update', function() {
+    renderRolodex(rolodex);
   });
 
+  // contactData.forEach(function(rawContact) {
+  //   var contact = new Contact(rawContact);
+  //   render(contact);
+  // });
+
   console.log("Working.");
+
+$('.btn-save').click(function(event) {
+  var formData = readContactForm();
+  console.log(formData);
+
+  var contact = new Rolodex(formData);
+  rolodex.add(contact);
+});
+
 });
