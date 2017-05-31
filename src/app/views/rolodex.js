17b19c0eb1df3229724a0b5e37d29104c09cf6d4
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-31 14:58:06 -0700

    created rolodex view file and put in import/export code

---
+++ src/app/views/rolodex.js
@@ -0,0 +1,11 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+import ContactView from '../views/contact_view';
+import Contact from '../models/contact';
+
+var RolodexView = Backbone.View.extend({
+
+});
+
+export default RolodexView;
