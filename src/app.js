e682dcf3046625a5cbfb894748ad610934461a0e
amb54 <ambarklund@hotmail.com>
2017-05-30 18:47:20 -0700

    Imported the underscore from the library underscore. Imported the model Contact. Created a collection of contacts with only one contact (contactData). Created an instance variable oneContact of the model Contact. Created the .ready block with two compiled templates that also is used.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 import $ from 'jquery';
+import _ from 'underscore';
+
+// import MODELS
+import Contact from 'app/models/contact'
+
+var contactData = [
+  {
+    name: 'Donald Duck',
+    phone: '206 111 1111',
+    email: 'donalds@email.com'
+  }
+]
+
+var oneContact = new Contact(contactData[0]);
+
+$(document).ready(function() {
+  var contactCardTemplate = _.template($('#tmpl-contact-card').html());
+  var generateHTML1 = contactCardTemplate(oneContact.toJSON());
+  $('#contact-cards').append(generateHTML1)
+
+  var contactDetailsTemplate = _.template($('#tmpl-contact-details').html());
+  var generateHTML2 = contactDetailsTemplate(oneContact.toJSON());
+  $('#contact-details').append(generateHTML2)
+});
