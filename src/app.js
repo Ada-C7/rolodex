3a0d8ddfeed2b924a8f2ee09dd4a632026c899ce
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-05-30 21:39:02 -0700

    single instance of Contact created from static data rendered using Underscore Template

--- src/app.js
+++ src/app.js
@@ -1,24 +1,43 @@
 import _ from 'underscore';
 import $ from 'jquery';
 
-import Contact from './models/postit';
+import Contact from 'app/models/contact';
+
+var contactTemplate;
 
 var contactData = [
   {
     name: 'Dara',
-    phoneNumber: '555-1212'
-    email:
+    phoneNumber: '555-1212',
+    email: 'dara@email.com'
   }, {
     name: 'Rachel',
-    phoneNumber: '207-20I-LUVU'
-    email:
+    phoneNumber: '207-20I-LUVU',
+    email: 'rachel@email.com'
   }, {
     name: 'Lynn',
-    phoneNumber: '555-1234'
-    email:
+    phoneNumber: '555-1234',
+    email: 'lynn@email.com'
   }, {
-    name: 'Jenni'
-    phoneNumber: '867-5309'
-    email:
-  }
-];
+    name: 'Jenni',
+    phoneNumber: '867-5309',
+    email: 'jenni@email.com'
+  }];
+
+var myContact = new Contact(contactData[0]);
+
+var render = function(contact) {
+  var jsonContact = myContact.toJSON();
+  var generatedHTML = contactTemplate(jsonContact);
+  console.log(generatedHTML);
+
+  $("#contact-cards").append(generatedHTML);
+};
+
+$(document).ready(function() {
+  console.log(myContact.get("name"));
+
+  contactTemplate = _.template($('#tmpl-contact-card').html());
+
+  render(myContact);
+});
