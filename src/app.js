e21c182f9c18788369be5e2e4f6ad7679466ae6c
Emma Claire Philippides <acme9898@gmail.com>
2017-05-31 10:53:14 -0700

    initial commit, wave 1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 import $ from 'jquery';
+import Contact from 'app/models/contact';
+import _ from "underscore";
+
+var myContact = new Contact({
+  name: "Leonardi Davinci",
+  phone_number: "(111)-111-1111",
+  email: "leodavinci@gioconda.com"
+});
+
+$(document).ready(function() {
+
+
+  console.log("Name: " + myContact.get("name") +" "+ "Phone Number: " + myContact.get("phone_number") +" "+ "Email: "+ myContact.get("email"));
+
+});
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -1,6 +1,12 @@
 import Backbone from 'backbone';
 
 const Contact = Backbone.Model.extend({
+
+  defaults: {
+    "name": "enter name",
+    "phone_number": "enter phone number",
+    "e-mail": "enter email"
+  }
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
 });
