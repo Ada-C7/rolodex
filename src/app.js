9e57fad2509cac3d359cf4658b4eef21c94ec3c5
Sofia Kim <sofiamhkim@gmail.com>
2017-05-31 16:00:01 -0700

    wave 2 collection for contacts

--- src/app.js
+++ src/app.js
@@ -2,14 +2,20 @@ import $ from 'jquery';
 import _ from 'underscore';
 
 import Contact from 'app/models/contact';
+import Rolodex from 'app/collections/rolodex';
 
 var contactCardTemplate;
 
-var myContact = new Contact({
+var contactData = [{
   name: "Sofia",
   email: "sofia@adadevelopers.com",
   phone: "222-222-2222"
-});
+},
+{
+  name: "Ting",
+  email: "ting@adadevelopers.com",
+  phone: "222-222-2222"
+}];
 
 
 var render = function(myContact){
@@ -21,10 +27,13 @@ var render = function(myContact){
 
 $(document).ready(function() {
 
-  console.log(myContact.get("name") +  " : " + myContact.get("email") + myContact.get("phone"));
-
   contactCardTemplate = _.template($('#tmpl-contact-card').html());
 
-  render(myContact);
+  contactData.forEach(function(contactData){
+    var contact = new Contact(contactData);
+    console.log(contact.get("name") +  " : " + contact.get("email") + contact.get("phone"));
 
+    render(contact);
+
+  });
 });
