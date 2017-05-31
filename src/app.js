d01068b86c895060357047dddff8c6cf2a21f9ee
Tamiko Terada <tamikodevo@gmail.com>
2017-05-30 17:01:31 -0700

    wave 1 complete w/Tehut

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
 import $ from 'jquery';
+import _ from 'underscore';
+import Contact from 'app/models/contact';
+
+var contactTemplate;
+
+var contactData = {
+  name: "Anacelia",
+  phone: "111-1111",
+  email: "anacelia@ugh.com"
+}
+
+var firstContact = new Contact(contactData);
+
+var render = function(contact) {
+  var myContact = contact.toJSON();
+  var generatedHTML = contactTemplate(myContact);
+  $('#contact-cards').append(generatedHTML);
+};
+
+$(document).ready(function(){
+  contactTemplate = _.template($("#tmpl-contact-card").html());
+
+  console.log("success!");
+  render(firstContact);
+
+  $("#contact-details").hide();
+});
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,11 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+  default: {
+    "name": "test name",
+    "phone": "555-5555",
+    "email": "person@earth.com"
+  }
 });
 
 export default Contact;
