82e00656df3caf5e4e60ccb87d846043b6bd489a
Marisol Lopez <lmarisolopez@gmail.com>
2017-05-31 16:40:12 -0700

    initial funcionality for wave 1

--- src/app.js
+++ src/app.js
@@ -1,3 +1,25 @@
 import $ from 'jquery';
 import _ from 'underscore';
-import Contact from './models/contact';
+import Contact from 'app/models/contact';
+
+var rolodexTemplate;
+
+var rolodexData = [{
+name: "Marisol",
+email: "mlopez@mail.com",
+phone: "562-320-9014"
+}];
+
+var myContact = new Contact(rolodexData[0]);
+
+var render = function(contact) {
+  var jsonContact = contact.toJSON();
+  var generatedHTML = rolodexTemplate(jsonContact);
+  console.log(generatedHTML);
+  $("#contact-cards").append(generatedHTML);
+};
+
+$(document).ready(function(){
+  rolodexTemplate = _.template($("#tmpl-contact-card").html());
+  render(myContact);
+})
