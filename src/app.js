3627c861a2b65740539a635e8b1b3a68401d9580
Emma Claire Philippides <acme9898@gmail.com>
2017-05-31 16:54:40 -0700

    view

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 import $ from 'jquery';
 import Contact from 'app/models/contact';
 import _ from "underscore";
+import Rolodex from 'app/collections/rolodex';
+import ContactView from 'app/views/contact_view.js';
 
 var contactCardTemplate;
+var contactList;
 
-var myContact = new Contact({
+var contactData = [{
   name: "Leonardi Davinci",
   phone: "(111)-111-1111",
   email: "leodavinci@gioconda.com"
-});
+},
+{
+  name: "Sir Walter Scott",
+  phone: "(111)-222-2222",
+  email: "sirscott@marmion.com"
+}];
 
 var render = function(contact) {
   var jsonContact = contact.toJSON();
@@ -17,11 +25,38 @@ var render = function(contact) {
   $('#contact-cards').append(generatedHTML);
 };
 
+var renderList = function(contactList) {
+  // Clear the unordered list
+  $('#contact-cards').empty();
+  // Iterate through the list rendering each Task
+  contactList.each(function(contact) {
+
+    var contactView = new ContactView({
+      model: contact,
+      template: _.template($('#tmpl-contact-details').html()),
+      tagname: 'li'
+    });
+
+    // Then render the TaskView
+    // And append the resulting HTML to the DOM.
+    // $('.todo-items').append(taskView.render().$el);
+  });
+
+};
+
 $(document).ready(function() {
 
-contactCardTemplate = _.template($('#tmpl-contact-card').html());
-render(myContact);
+  contactCardTemplate = _.template($('#tmpl-contact-card').html());
 
-  console.log("Name: " + myContact.get("name") +" "+ "Phone Number: " + myContact.get("phone") +" "+ "Email: "+ myContact.get("email"));
+  contactList = new Rolodex(contactData);
+  contactList.on("update", function(){
+    renderList(contactList);
+  });
 
+  // contactData.forEach(function(contactData){
+  //   var contact = new Contact(contactData);
+  //
+  //   console.log("Name: " + contact.get("name") +" "+ "Phone Number: " + contact.get("phone") +" "+ "Email: "+ contact.get("email"));
+
+  renderList(contactList);
 });
