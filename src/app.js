ddbf200e75547b3323443cea8c8510255975dce4
Sahana Murthy <s.murthy730.com>
2017-05-31 16:07:07 -0700

    Contact card appearing on screen. Contact object's information appropriately displaying on console.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 import $ from 'jquery';
+import _ from 'underscore';
+
+import Contact from './app/models/contact';
+
+var myContact = new Contact({
+  name: "Sahana",
+  email: "s.murthy730@yahoo.com",
+  phone: "707-293-8907"
+});
+
+var render = function(contact) {
+
+  var template_text = $('#tmpl-contact-card').html();
+
+  var templateObject = _.template(template_text);
+
+  var compiledHTML = templateObject(contact.toJSON());
+
+  $('#contact-cards').append(compiledHTML);
+};
+
+$(document).ready(function() {
+
+  console.log(myContact.get("name") + myContact.get("email") + myContact.get("phone"));
+  render(myContact);
+});
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -1,8 +1,7 @@
 import Backbone from 'backbone';
 
 const Contact = Backbone.Model.extend({
-  // This model should have the attributes for
-  // a single contact: name, phone number, and email.
+  
 });
 
 export default Contact;
