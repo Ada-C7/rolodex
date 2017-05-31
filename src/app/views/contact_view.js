9cef2af0d1fd1529e4dcb4552ea072b89c0870f1
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-05-31 12:43:58 -0700

    Initalize and render functions for ContactView

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,7 +1,21 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
 import Contact from '../models/contact.js';
 
-const ContactView = Backbone.View.extend({
+var ContactView = Backbone.View.extend({
+  initialize: function(params){
+    this.template = params.template;
+    this.listenTo(this.model, "change", this.render);
+  },
+  render: function(){
+    var compiledTemplate = this.template(this.model.toJSON());
+    this.$el.html(compiledTemplate);
+    return this;
+  },
+  events: {
+
+  }
 });
 
 export default ContactView;
