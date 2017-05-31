a9dabc99df6824a9430d0a77f77365fecd8aaa97
jjousun <jjousun@gmail.com>
2017-05-30 20:24:50 -0700

    Added Rolodex Collection and rendered

--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import Contact from './app/models/contact.js';
+import Rolodex from './app/collections/rolodex.js';
 
-var myContact = new Contact({
-  name: "Ada",
-  email: "ada@ada.org",
-  phone: 6172779728
-});
+// var myContact = new Contact({
+//   name: "Ada",
+//   email: "ada@ada.org",
+//   phone: 6172779728
+// });
+
+var contactsData = [
+  {
+    name: "Ada",
+    email: "ada@ada.org",
+    phone: 6172779728
+  },
+  {
+    name: "Grace",
+    email: "grace@ada.org",
+    phone: 6172779340
+  },
+  {
+    name: "Katherine",
+    email: "kat@ada.org",
+    phone: 6178358880
+  },
+  {
+    name: "ME",
+    email: "me@ada.org",
+    phone: 1000000000
+  }
+];
+
+var myRolodex = new Rolodex(contactsData);
 
 var render = function(Contact) {
   var templateText = $('#tmpl-contact-card').html();
@@ -18,8 +44,16 @@ var render = function(Contact) {
   $("#contact-cards").append(compiledHTML);
 };
 
+var renderList = function(Rolodex) {
+  $("contact-cards").empty();
+  Rolodex.each(function(Contact) {
+    render(Contact);
+  });
+};
+
 $(document).ready(function() {
   // console.log(myContact.get("name"));
-  render(myContact);
+  // render(myContact);
+  renderList(myRolodex);
 
 });
