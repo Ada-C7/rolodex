ba7b5e6b7e373825707fb6b8fb1c99de4dacc132
Anacelia Gillette <acgillette426@gmail.com>
2017-05-31 12:33:23 -0700

    Started on wave 2

--- src/app.js
+++ src/app.js
@@ -3,19 +3,33 @@ import ApplicationView from 'app/views/application_view';
 import $ from 'jquery';
 import _ from 'underscore';
 import Contact from './app/models/contact.js';
+import Rolodex from './app/collections/rolodex.js';
 
 var application = new Application();
-
-var myContact = new Contact({
-  name: "A name",
-  phone_num: "3057778888",
-  email: "email@email.com"
-});
 
 var appView = new ApplicationView({
   el: '#application',
   model: application
 });
+
+var contactData = [ {
+  name: "A name",
+  phone_num: "3057778888",
+  email: "email@email.com"
+}, {
+  name: "Bo",
+  phone_num: "2223334444",
+  email: "bo@bo.com"
+}];
+
+var contactList = new Rolodex(contactData);
+
+var renderList = function(contactList) {
+  $('#contact-cards').empty();
+  contactList.each(function(contact) {
+    render(contact);
+  });
+};
 
 var render = function(contact) {
   var templateText = $('#tmpl-contact-card').html();
@@ -29,5 +43,5 @@ var render = function(contact) {
 };
 
 $(document).ready(function() {
-  render(myContact);
+  renderList(contactList);
 });
