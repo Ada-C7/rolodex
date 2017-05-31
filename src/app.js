62f6cb3f28b4c921401d2b066c364bf7873e4441
Sofia Kim <sofiamhkim@gmail.com>
2017-05-31 10:53:45 -0700

    wave 1 created contact model and default data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import $ from 'jquery';
+import _ from 'underscore';
+
+import Contact from 'app/models/contact';
+
+var myContact = new Contact({
+  name: "Sofia",
+  e_mail: "sofia@adadevelopers.com",
+  phone_number: "222-222-2222"
+});
+
+$(document).ready(function() {
+
+
+  console.log(myContact.get("name") +  " : " + myContact.get("e_mail") + myContact.get("phone_number"));
+
+});
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,11 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+  defaults: {
+  "name": "Name",
+  "e_mail": "E-mail address",
+  "phone_number": "Phone number"
+}
 });
 
 export default Contact;
