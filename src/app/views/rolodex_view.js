ea7374f17ed59e2fb1df0b6e3ea5dbe1f3ec1df3
Jackie Watanabe <sushiigirl@gmail.com>
2017-05-31 15:21:07 -0700

    added initial code for rolodex_view.js

---
+++ src/app/views/rolodex_view.js
@@ -0,0 +1,11 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+import Contact from '../models/contact.js';
+import ContactView from './views/contact_view.js';
+
+var RolodexView = Backbone.View.extend({
+
+});
+
+export default RolodexView;
