e663ba09a9b51121cd81f76cd28067b2f1395ab8
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-30 20:34:53 -0700

    have single instance of contact working

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
-var application = new Application();
+import _ from 'underscore';
+import $ from 'jquery';
+import Contact from 'app/models/contact';
 
-var appView = new ApplicationView({
-  el: '#application',
-  model: application
+// var application = new Application();
+//
+// var appView = new ApplicationView({
+//   el: '#application',
+//   model: application
+// });
+
+// var templateContact;
+var templateCard;
+
+var rawContact = {
+  name: "cyn",
+  phone: "123-456-7890",
+  email: "cyn@test.com"
+};
+
+var renderContact = function(contact){
+  var generatedHTML = templateCard(contact.toJSON());
+  $('#contact-details').append(generatedHTML);
+};
+
+$(document).ready(function() {
+  // templateNameObj = _.template(template_name);
+  templateCard = _.template($('#tmpl-contact-details').html());
+
+  var contact = new Contact( rawContact );
+  renderContact(contact);
 });
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,11 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+  defaults: {
+    name: "-",
+    phone: "-",
+    email: "-"
+  }
 });
 
 export default Contact;
