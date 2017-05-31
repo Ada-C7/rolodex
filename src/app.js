84b90e1ce9f588c77bde17dae167afa368dc1824
EricaJCase <ericajcase@gmail.com>
2017-05-31 16:21:08 -0700

    added rolodex collection functionality

--- src/app.js
+++ src/app.js
@@ -2,21 +2,22 @@ import $ from 'jquery';
 import _ from 'underscore';
 
 import Contact from 'app/models/contact';
+import Rolodex from './app/collections/rolodex';
 var contactTemplate;
 
-var contactData= [
+var contactData = [
   {
     name: "TJ O'Pootertoot",
     email: "tj@pootertoots.com",
     phone: "666-666-6666"
   }, {
-    name: "Francisca Fernandez",
+    name: "Francesca Fernandez",
     email: "francesca@gmail.com",
     phone: "555-555-5555"
   }
 ];
 
-var newContact = new Contact(contactData[0]);
+var  rolodex = new Rolodex(contactData);
 
 var render = function(contact) {
   var generatedHTML = $(contactTemplate(contact.toJSON()));
@@ -28,14 +29,20 @@ var render = function(contact) {
   // });
 };
 
-console.log(newContact);
 
 $(document).ready(function() {
 
-  contactTemplate = _.template($("#tmpl-contact-details").html());
+  contactTemplate = _.template($("#tmpl-contact-card").html());
 
-  render(newContact)
+  var renderRolodex = function(rolodex) {
+    // Clear the unordered list
+    $('#contact-cards').empty();
 
+    rolodex.each(function(contact) {
+      render(contact);
+    });
+  };
 
+  renderRolodex(rolodex);
 
 });
