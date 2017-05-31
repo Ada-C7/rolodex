e438baba095daab4e287caf974095d375f1331d5
AlisonZ <alisonkzerbe@gmail.com>
2017-05-31 15:06:47 -0700

    iterating through collection of contacts and displaying

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import ApplicationView from 'app/views/application_view';
 import $ from 'jQuery';
 import _ from 'underscore';
 import Contact from './app/models/contact.js';
+import Rolodex from './app/collections/rolodex.js'
 
 var application = new Application();
 
@@ -11,11 +12,19 @@ var appView = new ApplicationView({
   model: application
 });
 
-var myContact = new Contact({
-    name: 'Mom',
-    phoneNumber: '123',
-    email: 'mom@gmail.com'
-})
+var contactData = [{
+    name: 'Drew',
+    phoneNumber: '304',
+    email: 'drew@gmail.com'
+},
+{
+    name: 'Dad',
+    phoneNumber: '987',
+    email: 'bob@gmail.com'
+}]
+
+var contactList = new Rolodex(contactData);
+
 
 var render = function(contact){
     var templateText = $('#tmpl-contact-card').html();
@@ -28,7 +37,10 @@ var render = function(contact){
 
 
 $(document).ready(function(){
-    console.log('hi there');
-    console.log(myContact);
-    render(myContact);
+    console.log('!!!!!!!!!!!************!!!!!!!!!!');
+    console.log(contactList);
+    // render(myContact);
+    contactList.each(function(contact){
+        render(contact);
+    });
 });
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,14 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+import Contact from '../models/contact.js';
+
 
 const ContactView = Backbone.View.extend({
+    initialize: function(params){
+        this.template = params.template;
+    }
+
 });
 
 export default ContactView;
