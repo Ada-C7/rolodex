8052f427aa36c8a4f70d92abf45121b28d26970b
Leesyruiz <leesyruiz@gmail.com>
2017-06-01 09:19:34 -0700

    added to views

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -2,7 +2,55 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 import $ from 'jquery';
 
-const ContactView = Backbone.View.extend({
-});
+var ContactView = Backbone.View.extend({
 
-export default ContactView;
+  tagname: "li"
+  template: _.template($('#contact-template').html()),
+
+  events: {
+    "click .toggle"   : "toggleDone",
+    "dblclick .view"  : "edit",
+    "click a.destroy" : "clear",
+    "keypress .edit"  : "updateOnEnter",
+    "blur .edit"      : "close"
+  },
+
+  initialize: function() {
+    this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+  },
+
+  render: function(){
+    this.$el.html(this.template(this.model.toJSON()));
+    this.$el.toggleClass('save', this.model.get('save'));
+    this.input = this.$('.edit');
+    return this;
+  },
+
+  toggleDone: function(){
+    this.model.toggle();
+  },
+
+    edit: function(){
+      this.$el.addClass("editing");
+      this.input.focus();
+    },
+
+    close: function(){
+      var value = this.input.val();
+      if (!value) {
+        this.clear();
+      } else {
+        this.model.save({titleL value});
+        this.$el.removeClass("editing");
+      }
+    },
+
+    updateOnEnter: function(e){
+      if (e.keyCode == 13) this.close();
+    },
+
+)
+
+
+  export default ContactView;
