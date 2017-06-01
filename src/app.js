0385f1ac9033306609c355f692da666b8b6d75b0
Haby Randall <sow.haby@gmail.com>
2017-06-01 00:13:51 -0700

    Wave 1 completed

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import Contact from './app/models/contact';
+
+var myContact = new Contact({
+  name: "Haby",
+  email: "haby@gmail.com",
+  phone: "614-517-6803"
+});
+
+$(document).ready(function() {
+  // select template
+  var templateText = $('#tmpl-contact-card').html();
+  //get compiled template object
+  var templateObject = _.template(templateText);
+  // use template and model data to generate obj into html
+  var compiledHTML = templateObject(myContact.toJSON());
+
+  $('#contact-cards').append(compiledHTML);
+});
