2a16b308b2f105e6ad59008a79f4a1606691105e
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-06-01 15:28:13 -0700

    Styling

--- src/app.js
+++ src/app.js
@@ -8,63 +8,76 @@ import RolodexView from 'app/views/rolodex_view.js';
 var myContacts = [
   { name: "Dwight Schrute",
     email: "schrutefarms@gmail.com",
-    phone: "111-111-1111"
+    phone: "111-111-1111",
+    image: "imgs/dwight.png"
   },
   {
     name: "Jim Halpert",
     email: "jim@athlead.com",
-    phone: "222-222-2222"
+    phone: "222-222-2222",
+    image: "imgs/jim.png"
   },
   {
     name: "Pam Beasley",
     email: "Pam@prattinstitute.com",
-    phone: "333-333-3333"
+    phone: "333-333-3333",
+    image: "imgs/pam.png"
   },
   {
     name: "Michael Scott",
     email: "mscott@greatscottproductions.com",
-    phone: "444-444-4444"
+    phone: "444-444-4444",
+    image: "imgs/michael.png"
   },
   {
     name: "Kelly Kapoor",
     email: "kkapoor@businessbitch.com",
-    phone: "555-555-5555"
+    phone: "555-555-5555",
+    image: "imgs/kelly.png"
   },
-  {
-    name: "Ryan Howard",
-    email: "rhoward@wupf.edu",
-    phone: "666-666-6666"
-  },
+  // {
+  //   name: "Ryan Howard",
+  //   email: "rhoward@wupf.edu",
+  //   phone: "666-666-6666",
+  //   image: "imgs/ryan.png"
+  // },
   {
     name: "Andy Bernard",
     email: "narddog@cornell.edu",
-    phone: "777-777-7777"
+    phone: "777-777-7777",
+    image: "imgs/andy.png"
   },
   {
     name: "Kevin Malone",
     email: "bigbog69@fanduel.com",
-    phone: "888-888-8888"
+    phone: "888-888-8888",
+    image: "imgs/kevin.png"
+
   },
   {
     name: "Angela Martin",
     email: "msmartin@lipton4PA.com",
-    phone: "999-999-9999"
+    phone: "999-999-9999",
+    image: "imgs/angela.png"
   },
-  {
-    name: "Toby Flenderson",
-    email: "chadflenderson@flendersonfiles.com",
-    phone: "1-111-111-1111"
-  },
+  // {
+  //   name: "Toby Flenderson",
+  //   email: "chadflenderson@flendersonfiles.com",
+  //   phone: "1-111-111-1111",
+  //   image: "imgs/toby.png"
+  // },
   {
     name: "Creed Bratton",
     email: "notcreedbratton@creedthoughts.gov",
-    phone: "2-222-222-2222"
-  },
-  {
-    name: "David Wallace",
-    email: "dwallace@suckit.com",
-    phone: "3-333-333-3333"
+    phone: "2-222-222-2222",
+    image: "imgs/creed.png"
   }
+  // {
+  //   name: "David Wallace",
+  //   email: "dwallace@suckit.com",
+  //   phone: "3-333-333-3333",
+  //   image: "imgs/david.png"
+  // }
 ];
 
 var myRolodex = new Rolodex(myContacts);
@@ -81,5 +94,10 @@ $(document).ready(function() {
 
   $('.scroll_navigation ul li').on('click', function(){
     $('html,body').animate({scrollTop: $(this).offset().top}, 800);
+  });
+
+
+  $('.staff-pic').hover(function(){
+    $(this).toggleClass('grayscale');
   });
 });
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -5,7 +5,20 @@ var Contact = Backbone.Model.extend({
   defaults: {
     name: "YOUR NAME",
     phone: "1-800-555-5555",
-    email: "youremail@gmail.com"
+    email: "youremail@gmail.com",
+    image: "imgs/andy.png"
+  },
+
+  getRandomImage: function(){
+    var officeImages = [
+      "imgs/andy.png",
+      "imgs/angela.png",
+      "imgs/creed.png",
+      "imgs/dwight.png",
+      "imgs/gabe.png",
+      "imgs/pam.png"
+    ];
+    return _.sample(officeImages);
   },
 
   logStatus: function(){
