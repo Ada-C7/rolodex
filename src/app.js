ed87208b8fd4a09d3740da1e2afd9e4d91d13144
Haby Randall <sow.haby@gmail.com>
2017-06-01 00:32:17 -0700

    Starting the Rolodex collection, and looping in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import Contact from './app/models/contact';
+import Rolodex from './app/collections/rolodex';
+
+
+var contactData = [
+  {
+    name: "Mina",
+    email: "Mina@yahoo.com",
+    phone: "614-516-2473"
+  }, {
+    name: "Rosco",
+    email: "Rosco@yahoo.com",
+    phone: "614-342-4223"
+  }
+];
+
+var myContactList = new Rolodex(contactData);
 
 var myContact = new Contact({
   name: "Haby",
   email: "haby@gmail.com",
   phone: "614-517-6803"
 });
+
+var renderList = function(contactList) {
+  $('#contact-cards').empty();
+
+  contactList.each(function(task) {
+    
+  });
+};
+
+
 
 $(document).ready(function() {
   // select template
