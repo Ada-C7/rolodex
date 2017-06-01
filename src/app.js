6a93f8c4493104f07ed755416cedf7a40c550830
Cara Comfort <cara.comfort10@gmail.com>
2017-06-01 15:54:47 -0700

    Add static data and render one instance using underscore

--- src/app.js
+++ src/app.js
@@ -1,1 +1,50 @@
 import $ from 'jquery';
+import _ from 'underscore';
+import Contact from './app/models/contact';
+
+
+var contactData = [
+  {
+    name: 'Richard Hendricks',
+    email: 'hendricks@piedpipper.org',
+    number: '(650)111-1111'
+  },
+  {
+    name: 'Erlich Bachman',
+    email: 'bachmanity.net',
+    number: '(650)111-1111'
+  },
+  {
+    name: 'Big Head',
+    email: 'bighetti@stanford.edu',
+    number: '(650)111-1111'
+  },
+  {
+    name: 'Bertram Gilfoyle',
+    email: 'gilfoyle@piedpipper.org',
+    number: '(650)111-1111'
+  },
+  {
+    name: 'Dinesh Chugtai',
+    email: 'chugtai@seefood.org',
+    number: '(650)111-1111'
+  },
+  {
+    name: 'Monica Hall',
+    email: 'hall@raviga.org',
+    number: '(650)111-1111'
+  },
+  {
+    name: 'Jian Yang',
+    email: 'yang@seefood.org',
+    number: '(650)111-1111'
+  }
+];
+
+$(document).ready(function() {
+  var contactTemplate = _.template($('#tmpl-contact-card').html());
+
+  var contact = new Contact(contactData[0]);
+  var generatedHTML = contactTemplate(contact.toJSON());
+  $('#contact-cards').append(generatedHTML);
+});
