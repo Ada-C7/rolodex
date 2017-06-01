73dfb8973a05136a0b0fce8e8f404d9fc066a30b
Lynn Trickey <lynn.trickey@gmail.com>
2017-06-01 10:10:04 -0700

    Created Rolodex View

---
+++ src/app/views/rolodex_view.js
@@ -0,0 +1,6 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+import Contact from '../models/contact.js';
+import Rolodex from './app/collections/rolodex.js';
+import ContactView from './app/views/contact_view.js';
