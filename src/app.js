1326a0e8dc998be2685dc054338745e1de644565
Alix Hamilton <alix.hamilton@gmail.com>
2017-05-31 17:11:19 -0700

    can create contact cards

--- src/app.js
+++ src/app.js
@@ -1,1 +1,45 @@
 import $ from 'jquery';
+import _ from 'underscore';
+import Application from './app/models/application.js';
+import ApplicationView from './app/views/application_view.js';
+import Contact from './app/models/contact.js';
+import Rolodex from './app/collections/rolodex.js';
+import ContactView from './app/views/contact_view.js';
+import RolodexView from './app/views/rolodex_view.js';
+
+var application = new Application ();
+var appview = new ApplicationView({
+  el: '#application',
+  model: application
+});
+
+var contactData = [
+  {
+    name: 'Alix',
+    email: 'boom@boom.com',
+    phoneNumber: '5555555555'
+  }, {
+    name: 'Hammy',
+    email: 'ham@boom.com',
+    phoneNumber: '5555565555'
+  }, {
+    name: 'Hilary',
+    email: 'hilla@boom.com',
+    phoneNumber: '5575555555'
+  }
+];
+
+var rolodex = new Rolodex(contactData);
+
+$(document).ready(function () {
+
+  var rolodexView = new RolodexView({
+    model: rolodex,
+    template: _.template($('#tmpl-contact-card').html()),
+    el: '#application'
+  });
+
+  rolodexView.render();
+
+  console.log(rolodex);
+});
