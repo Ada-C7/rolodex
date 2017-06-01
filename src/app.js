a50ba63045e147f80305e63ab39c9e47a0a0a538
Lynn Trickey <lynn.trickey@gmail.com>
2017-06-01 10:06:27 -0700

    Created Contact view for one contact

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 import _ from 'underscore';
 import Contact from './app/models/contact.js';
 import Rolodex from './app/collections/rolodex.js';
+import ContactView from './app/views/contact_view.js';
 
 var application = new Application();
 
@@ -18,69 +19,80 @@ var myContact = new Contact({
   email: "lynn@test.com"
 });
 
-var contactData = [
-  {
-    name: "Mom",
-    phone: 2063658186,
-    email: "mom@test.com"
-  },
-  {
-    name: "Lynn",
-    phone: 2062401029,
-    email: "lynn@test.com"
-  },
-  {
-    name: "Annie",
-    phone: 2061234589,
-    email: "Annie@test.com"
-  }
-];
+// var contactData = [
+//   {
+//     name: "Mom",
+//     phone: 2063658186,
+//     email: "mom@test.com"
+//   },
+//   {
+//     name: "Lynn",
+//     phone: 2062401029,
+//     email: "lynn@test.com"
+//   },
+//   {
+//     name: "Annie",
+//     phone: 2061234589,
+//     email: "Annie@test.com"
+//   }
+// ];
 
-var myRolodex = new Rolodex(contactData);
+// var myRolodex = new Rolodex(contactData);
 
-var render = function(contact) {
-  var templateText = $("#tmpl-contact-card").html();
+// var render = function(contact) {
+//   var templateText = $("#tmpl-contact-card").html();
+//
+//   var templateObject = _.template(templateText);
+//
+//   var compiledHTML = templateObject(contact.toJSON());
+//
+//   $("#contact-cards").append(compiledHTML);
+// };
 
-  var templateObject = _.template(templateText);
+// var renderCollection = function(collection) {
+//   $("#contact-cards").empty();
+//   collection.each(function(contact) {
+//     render(contact);
+//   });
+// };
 
-  var compiledHTML = templateObject(contact.toJSON());
-
-  $("#contact-cards").append(compiledHTML);
-};
-
-var renderCollection = function(collection) {
-  $("#contact-cards").empty();
-  collection.each(function(contact) {
-    render(contact);
-  });
-};
-
-var getFormData = function() {
-  var formName = $("#contact-name").val();
-  $("#contact-name").val("");
-  var formEmail = $("#contact-email").val();
-  $("#contact-email").val("");
-  var formPhone = $("#contact-phone").val();
-  $("#contact-phone").val("");
-  return {
-    name: formName,
-    email: formEmail,
-    phone: formPhone
-  };
-};
+// var getFormData = function() {
+//   var formName = $("#contact-name").val();
+//   $("#contact-name").val("");
+//   var formEmail = $("#contact-email").val();
+//   $("#contact-email").val("");
+//   var formPhone = $("#contact-phone").val();
+//   $("#contact-phone").val("");
+//   return {
+//     name: formName,
+//     email: formEmail,
+//     phone: formPhone
+//   };
+// };
 
 $(document).ready(function() {
-  $("#contact-details").hide();
-  renderCollection(myRolodex);
+  // $("#contact-details").hide();
 
-  myRolodex.on("update", function() {
-    renderCollection(myRolodex);
+  var myContactView = new ContactView({
+    model: myContact,
+    template: _.template($("#tmpl-contact-card").html()),
+    tagName: 'li'
   });
-
-  $("h3.btn-save").click(function() {
-    var formData = getFormData();
-    var newContact = new Contact(formData);
-    myRolodex.add(newContact);
-  });
+  console.log("before render");
+  console.log(myContactView.render().el);
+  $('#contact-cards').append(myContactView.render().el);
 
 });
+
+
+// renderCollection(myRolodex);
+//
+// myRolodex.on("update", function() {
+//   renderCollection(myRolodex);
+// });
+//
+// $("h3.btn-save").click(function() {
+//   var formData = getFormData();
+//   var newContact = new Contact(formData);
+//   myRolodex.add(newContact);
+// });
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,19 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+import Contact from '../models/contact.js';
 
 const ContactView = Backbone.View.extend({
+  initialize: function(params) {
+    this.template = params.template;
+    console.log(params);
+  },
+  render: function() {
+    var compiledTemplate = this.template(this.model.toJSON());
+    this.$el.html(compiledTemplate);
+    return this;
+  }
+
 });
 
 export default ContactView;
