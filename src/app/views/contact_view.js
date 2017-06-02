c640158be4f3eea8d779aa650f360b46b8c56eab
Haby Randall <sow.haby@gmail.com>
2017-06-02 15:53:04 -0700

    Modal now popping up! Just need to hide it

--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -2,10 +2,13 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 import $ from 'jquery';
 import Contact from '../models/contact';
+// import RolodexView from ''
 
 const ContactView = Backbone.View.extend({
   initialize: function(params) {
     this.template = params.template;
+    this.contact = params.contact;
+    // console.log("This is the contact params" + params.contact);
 
 
     this.listenTo(this.model, 'change', this.render);
@@ -15,6 +18,16 @@ const ContactView = Backbone.View.extend({
 
     this.$el.html(compiledTemplate);
     return this;
+  },
+  events: {
+    'click li' : 'showModal'
+  },
+  showModal: function() {
+    this.trigger("selected", this);
+//     var modalHtml = this.modalTemplate(this.model.toJSON());
+// console.log("HEEEYY " + modalHtml);
+//     $('.contact-card').append(modalHtml);
+    // this.modal.show();
   }
 });
 
