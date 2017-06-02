d98ea89a0bac5035c4a2fe8dd3c02376fd00a424
Haby Randall <sow.haby@gmail.com>
2017-06-02 11:26:00 -0700

    Working on rolodex view initialize

--- src/app/views/rolodex_view.js
+++ src/app/views/rolodex_view.js
@@ -6,8 +6,29 @@ import ContactView from 'views/contact_view';
 const RolodexView = Backbone.View.extend({
   initialize: function(params) {
   console.log("this is " + params);
-   this.template = _.template($('#tmpl-contact-card').html());
-  }
+   this.template = params.template;
+   this.model = Contact;
+
+   this.input = {
+     name: this.$('.contact-form input[name="name"]'),
+     phone: this.$('.contact-form input[name="phone"]'),
+     email: this.$('.contact-form input[name="email"]')
+   };
+ },
+ render: function() {
+   this.$("#contact-cards").empty();
+   var that = this;
+
+   this.model.each(function(contact) {
+     var newContactView = new Contactview({
+       model: contact,
+       template: that.template,
+       tagName: 'li'
+     });
+     that.$('#contact-cards').append(newContactView.render().$el);
+   });
+   return this;
+ }
 });
 
 export default RolodexView;
