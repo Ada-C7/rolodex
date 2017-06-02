76f84fe94878481617865b095dfcff4651a7cf8c
Allison Northrop <agnorthrop@gmail.com>
2017-06-02 10:48:56 -0700

    got the foundation of the files set up

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 import $ from 'jquery';
+import _ from 'underscore';
+
+import Contact from 'app/models/contact';
+import Rolodex from './app/collections/rolodex';
+
+import ContactView from './app/views/contact_view.js';
+
+var contactTemplate
+var rolodex
+
+var contactData = [
+  {
+    name: "Humphrey Bogart",
+    email: "we'llalwayshaveparis@gmail.com",
+    phone_number: "555-9382"
+  }
+];
+
+$(document).ready(function() {
+  contactTemplate = _.template($("#tmpl-contact-card").html());
+  rolodex = new Rolodex(contactData);
+
+
+});
--- src/app/views/contact_view.js
+++ src/app/views/contact_view.js
@@ -1,6 +1,23 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+import Task from '../models/contact.js';
 
 const ContactView = Backbone.View.extend({
+  tagName: 'li',
+  className: "contact-item column column-block",
+  initialize: function(params) {
+    this.template = params.template;
+    this.listenTo(this.model, "change", this.render);
+  },
+
+  render:function() {
+    var compiledTemplate = this.template(this.model.toJSON());
+    this.$el.html(compiledTemplate);
+    return this;
+  }
+
+  //come back fro delete?
 });
 
 export default ContactView;
