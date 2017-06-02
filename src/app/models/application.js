ce291d1ef27ca6b47c1bf68589ec07354f26a32a
Leesyruiz <leesyruiz@gmail.com>
2017-06-02 10:09:21 -0700

    contact added to application.js

--- src/app/models/application.js
+++ src/app/models/application.js
@@ -2,6 +2,9 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 import $ from 'jquery';
 
+import Contact from 'app/models/contact';
+
+
 const Application = Backbone.Model.extend({
   // This model represents the overall application.
 });
