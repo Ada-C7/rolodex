edd8100a9e8217622738c4205164f58381228263
amb54 <ambarklund@hotmail.com>
2017-06-02 09:39:00 -0700

    Created a new instansvariable of the RolodexView and rendered it. Commented out all codewith functionality now incorporated int the views RolodexView and ContactView.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ import _ from 'underscore';
 import Contact from 'app/models/contact'
 import Rolodex from 'app/collections/rolodex'
 
+// import VIEWS
+import ContactView from 'app/views/contact_view'
+import RolodexView from 'app/views/rolodex_view'
+
 // GLOBAL variables
 var contactCardTemplate;
 var contactDetailsTemplate;
@@ -27,69 +31,88 @@ var contactData = [
 
 var oneContact = new Contact(contactData[0]);
 
-var readForm = function(){
-  var formName = $('#name').val();
-  $('#name').val('');
+// var readForm = function(){
+//   var formName = $('#name').val();
+//   $('#name').val('');
+//
+//   var formEmail = $('#email').val();
+//   $('#email').val('');
+//
+//   var formPhone = $('#phone').val();
+//   $('#phone').val('');
+//
+//   var formData = {};
+//   if (formName && formName != "") {
+//     formData["name"] = formName
+//   }
+//   if (formEmail && formEmail != "") {
+//     formData["email"] = formEmail
+//   }
+//   if (formPhone && formPhone != "") {
+//     formData["phone"] = formPhone
+//   }
+//   return formData;
+// };
 
-  var formEmail = $('#email').val();
-  $('#email').val('');
+// var renderContact = function(contact){
+//   var jsonContact = contact.toJSON();
+//   var generateHTML = contactCardTemplate(jsonContact);
+//   $('#contact-cards').append(generateHTML);
+// };
 
-  var formPhone = $('#phone').val();
-  $('#phone').val('');
 
-  var formData = {};
-  if (formName && formName != "") {
-    formData["name"] = formName
-  }
-  if (formEmail && formEmail != "") {
-    formData["email"] = formEmail
-  }
-  if (formPhone && formPhone != "") {
-    formData["phone"] = formPhone
-  }
-  return formData;
-};
-
-var renderContact = function(contact){
-  var jsonContact = contact.toJSON();
-  var generateHTML = contactCardTemplate(jsonContact);
-  $('#contact-cards').append(generateHTML);
-};
-
-var renderRolodex = function(rolodexList){
-  $('#contact-cards').empty();
-  rolodexList.each(function(contact){
-    renderContact(contact);
-  });
-};
+// REMOVE use VIEW instead ******************
+// var renderRolodex = function(rolodexList){
+//   $('#contact-cards').empty();
+//   rolodexList.each(function(contact){
+//     // renderContact(contact);
+//     var contactView = new ContactView({
+//       model: contact,
+//       template: _.template($('#tmpl-contact-card').html())
+//     });
+//     $('#contact-cards').append(contactView.render().$el);
+//   });
+// };
+  // *****************************************
 
 
 
 $(document).ready(function() {
   contactCardTemplate = _.template($('#tmpl-contact-card').html());
+  contactDetailsTemplate = _.template($('#tmpl-contact-details').html());
   // var generateHTML1 = contactCardTemplate(oneContact.toJSON());
   // $('#contact-cards').append(generateHTML1);
   rolodexList = new Rolodex(contactData);
-  rolodexList.on('update', function(){
-    renderRolodex(rolodexList);
-  })
-  renderRolodex(rolodexList);
 
-  $('.btn-save').click(function(event){
-    var formData = readForm();
-    var newContact = new Contact(formData);
-    rolodexList.add(newContact);
+  // REMOVE use VIEW instead ******************
+  // rolodexList.on('update', function(){
+  //   renderRolodex(rolodexList);
+  // })
+  // renderRolodex(rolodexList);
+
+  // $('.btn-save').click(function(event){
+  //   var formData = readForm();
+  //   var newContact = new Contact(formData);
+  //   rolodexList.add(newContact);
+  // });
+  // *****************************************
+
+  var rolodexView = new RolodexView({
+    cardTemplate: _.template($('#tmpl-contact-card').html()),
+    detailTemplate: _.template($('#tmpl-contact-details').html()),
+    model: rolodexList,
+    el: $('#application')
   });
+  rolodexView.render();
+
+
+  $('#contact-details').hide()
 
 
 
-  contactDetailsTemplate = _.template($('#tmpl-contact-details').html());
-  var generateHTML2 = contactDetailsTemplate(oneContact.toJSON());
-  $('#contact-details').append(generateHTML2);
-
-
-
-
+  // contactDetailsTemplate = _.template($('#tmpl-contact-details').html());
+  // var generateHTML2 = contactDetailsTemplate(oneContact.toJSON());
+  // $('#contact-details').append(generateHTML2);
 
 
 });
