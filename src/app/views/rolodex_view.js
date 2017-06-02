a97ec669a1f0e188ad0bd2ba4ff7912567d90186
Sahana Murthy <s.murthy730.com>
2017-06-02 14:48:50 -0700

    Events from app.js refactored into rolodex_view

--- src/app/views/rolodex_view.js
+++ src/app/views/rolodex_view.js
@@ -7,7 +7,9 @@ import ContactView from './contact_view.js';
 var RolodexView = Backbone.View.extend({
   initialize: function(params) {
     this.template = params.template;
+    this.listenTo(this.model, "update", this.render);
   },
+
   render: function() {
     this.$('#contact-cards').empty();
     var that = this;
@@ -21,6 +23,42 @@ var RolodexView = Backbone.View.extend({
       that.$('#contact-cards').append(contactView.render().$el);
     });
     return this;
+  },
+
+  events: {
+    'click .btn-save': 'addContact',
+    'click .btn-cancel': 'clearForm'
+  },
+
+  clearForm: function() {
+    $('#name').val('');
+    $('#email').val('');
+    $('#phone').val('');
+  },
+
+  addContact: function(event) {
+    var formData = this.readNewContactForm();
+    console.log(formData);
+    this.model.add(formData);
+  },
+
+  readNewContactForm: function() {
+    var formName = $('#name').val();
+    var formEmail = $('#email').val();
+    var formPhone = $('#phone').val();
+    this.clearForm();
+
+    var formData = {};
+    if (formName && formName != "") {
+      formData["name"] = formName
+    }
+    if (formEmail && formEmail != "") {
+      formData["email"] = formEmail
+    }
+    if (formPhone && formPhone != "") {
+      formData["phone"] = formPhone
+    }
+    return formData;
   }
 });
 
