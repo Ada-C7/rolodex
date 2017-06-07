bd7d5b795f8df733986f922b8db9f0ae5b703b2d
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-06-07 09:34:39 -0700

    clean up app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 import $ from 'jquery';
+import _ from 'underscore';
+
+import Contact from 'app/models/contact';
+
+var contactTemplate;
+
+
+$(document).ready(function() {
+  contactTemplate = _.template($("tmpl-contact-card").html());
+  contactDetails = _.template($("tmpl-contact-details").html());
+});
--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -3,6 +3,11 @@ import Backbone from 'backbone';
 const Contact = Backbone.Model.extend({
   // This model should have the attributes for
   // a single contact: name, phone number, and email.
+  defaults: {
+    "name": "John Smith",
+    "email": "email@email.com",
+    "phone": "123-456-7890",
+  }
 });
 
 export default Contact;
