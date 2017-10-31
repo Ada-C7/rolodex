f2c9a0673a4bc6e1012da2e6b06b0eda30b9d3b4
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-10-30 19:33:59 -0700

    Updated images

--- src/app/models/contact.js
+++ src/app/models/contact.js
@@ -2,12 +2,29 @@ import Backbone from 'backbone';
 
 var Contact = Backbone.Model.extend({
 
+  getImage: function(){
+    var images = [
+      "imgs/andy.png",
+      "imgs/angela.png",
+      "imgs/creed.png",
+      "imgs/dwight.png",
+      "imgs/gabe.png",
+      "imgs/jim.png",
+      "imgs/kelly.png",
+      "imgs/michael.png",
+      "imgs/pam.png"
+    ];
+    return _.shuffle(images)[0];
+  },
+
   defaults: {
     name: "YOUR NAME",
     phone: "1-800-555-5555",
     email: "youremail@gmail.com",
-    image: "imgs/andy.png"
+    image: "imgs/pam.png"
+    // getImage()
   }
+
 
 });
 
